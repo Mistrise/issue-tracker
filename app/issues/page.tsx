7b54195f1d74fa77d0d417f08ef98818176045b0
Alex <alekseiivanov.2016@inbox.ru>
2024-03-25 18:42:18 +0300

    creating layout for new issue

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import {Button} from "@radix-ui/themes";
+import Link from "next/link";
 
 const IssuePage = () => {
     return (
         <div>
-            <Button className="m-5 p-5">New Issue</Button>
+            <Button className="m-5 p-5">
+                <Link href='/issues/new'>New Issue</Link>
+            </Button>
         </div>
     );
 };
