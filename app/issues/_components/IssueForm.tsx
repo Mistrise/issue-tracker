c03f56888de12c45dea61ce0dd68b51a1f42b9c1
aleksejivanov <alekseiivanov.2016@mail.ru>
2024-04-30 22:52:11 +0300

    build edit issue

---
+++ app/issues/_components/IssueForm.tsx
@@ -0,0 +1,58 @@
+'use client'
+import React, {useState} from 'react';
+import {Button, Callout, TextArea, TextField} from "@radix-ui/themes";
+import "easymde/dist/easymde.min.css";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import {useRouter} from "next/navigation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {createIssueSchema} from "@/app/validationSchema";
+import {z} from "zod";
+import ErrorMessage from "@/app/components/ErrorMessage";
+import Spinner from "@/app/components/Spinner";
+import {Issue} from "@prisma/client";
+
+interface Props {
+    issue?: Issue
+}
+
+type IssueFormData =  z.infer<typeof createIssueSchema>
+const IssueForm = ({issue}: Props) => {
+    const router = useRouter()
+    const { register, handleSubmit, formState: { errors } } = useForm<IssueFormData>({
+        resolver: zodResolver(createIssueSchema)
+    })
+    const [error, setError] = useState('')
+    const [isSubmitting, setIsSubmitting] = useState(false)
+
+    const onSubmit = handleSubmit( async (data) => {
+        try {
+            setIsSubmitting(true)
+            await axios.post('/api/issues', data)
+            router.push('/issues')
+        } catch (e) {
+            setIsSubmitting(false)
+            setError('A unexpected error occurred')
+        }
+    })
+
+
+    return (
+        <div className='max-w-xl p-4 mx-auto text-center'>
+            {error && <Callout.Root size="3" color="red" className='mb-2'>
+                <Callout.Text>
+                    {error}
+                </Callout.Text>
+            </Callout.Root>}
+            <form className='space-y-3' onSubmit={onSubmit}>
+                <TextField.Root defaultValue={issue?.title} placeholder='title' {...register('title')} />
+                <ErrorMessage>{errors.title?.message}</ErrorMessage>
+                <TextArea size="3" defaultValue={issue?.description} placeholder='description' {...register('description')}/>
+                <ErrorMessage>{errors.description?.message}</ErrorMessage>
+                <Button disabled={isSubmitting}>Submit New Issue{isSubmitting && <Spinner></Spinner>}</Button>
+            </form>
+        </div>
+    );
+};
+
+export default IssueForm;
