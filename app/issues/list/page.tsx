372227a0190afeb3f3db30d3ff3bd419a87db179
aleksejivanov <alekseiivanov.2016@mail.ru>
2024-05-02 15:38:12 +0300

    remove duplicate skeletons

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import {Table} from "@radix-ui/themes";
+import prisma from "@/prisma/client";
 import IssueStatusBadge from "@/app/components/IssueStatusBadge";
-import Skeleton from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
-import IssuesAction from "@/app/issues/IssuesAction";
+import IssuesAction from "@/app/issues/list/IssuesAction";
+import Link from '@/app/components/Link'
 
-const LoadingIssuesPage = () => {
-    const issues = [1,2,3,4,5]
+const IssuePage = async () => {
+    const issues = await prisma.issue.findMany()
 
     return (
-        <>
+        <div>
             <IssuesAction/>
             <Table.Root variant="surface">
                 <Table.Header>
@@ -22,26 +22,28 @@ const LoadingIssuesPage = () => {
                     </Table.Row>
                 </Table.Header>
                 <Table.Body>
-                    {issues.map((issue) => (
-                        <Table.Row key={issue}>
+                    {issues.map(issue => (
+                        <Table.Row key={issue.id}>
                             <Table.Cell>
-                                <Skeleton/>
+                                <Link href={`/issues/${issue.id}`}>
+                                    {issue.title}
+                                </Link>
                                 <div className="block md:hidden">
-                                    <Skeleton/>
+                                    <IssueStatusBadge status={issue.status}/>
                                 </div>
                             </Table.Cell>
                             <Table.Cell className="hidden md:table-cell">
-                                <Skeleton/>
+                                <IssueStatusBadge status={issue.status}/>
                             </Table.Cell>
-                            <Table.Cell className="hidden md:table-cell">
-                                <Skeleton/>
-                            </Table.Cell>
+                            <Table.Cell className="hidden md:table-cell">{issue.createdAt.toDateString()}</Table.Cell>
                         </Table.Row>)
                     )}
                 </Table.Body>
             </Table.Root>
-        </>
+        </div>
     );
 };
 
-export default LoadingIssuesPage;
+export const dynamic = 'force-dynamic'
+
+export default IssuePage;
