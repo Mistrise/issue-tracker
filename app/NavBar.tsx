293104baca7d1d3f7d0d2a5f0746ade9d83cf33c
Alex <alekseiivanov.2016@inbox.ru>
2024-05-16 10:40:56 +0300

    add login and logout links

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -4,12 +4,15 @@ import { FaBugs } from "react-icons/fa6";
 import Link from "next/link";
 import classnames from "classnames"
 import {usePathname} from "next/navigation";
+import {useSession} from "next-auth/react";
+import {Box} from "@radix-ui/themes";
 
 const NavBar = () => {
     const links = [
         {label: 'Dashboard', href: '/'},
         {label: 'Issues', href: '/issues/list'},
     ]
+    const {status, data: session} = useSession()
     const pathName = usePathname()
 
     return (
@@ -20,19 +23,25 @@ const NavBar = () => {
                 </Link>
                 <ul className='flex space-x-6'>
                     {links.map(link =>
-                        <Link key={link.href}
-                              className={
-                                classnames({
-                                    'text-zinc-500': pathName !== link.href,
-                                    'text-zinc-900': pathName === link.href,
-                                    'hover:text-zinc-800 transition-colors': true
-                                })
-                              }
-                              href={link.href}>
-                            {link.label}
-                        </Link>
+                        <li key={link.href}>
+                            <Link
+                                  className={
+                                    classnames({
+                                        'text-zinc-500': pathName !== link.href,
+                                        'text-zinc-900': pathName === link.href,
+                                        'hover:text-zinc-800 transition-colors': true
+                                    })
+                                  }
+                                  href={link.href}>
+                                {link.label}
+                            </Link>
+                        </li>
                     )}
                 </ul>
+            <Box>
+                {status === 'authenticated' && <Link href={'/api/auth/signout'}>Logout</Link>}
+                {status === 'unauthenticated' && <Link href={'/api/auth/signin'}>Login</Link>}
+            </Box>
         </nav>
 );
 };
