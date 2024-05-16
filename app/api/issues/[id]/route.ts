bf82b94c7ccb0491f5837d3c8c738e3d9ce0c3ea
Alex <alekseiivanov.2016@inbox.ru>
2024-05-16 13:17:36 +0300

    adding authorization

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,10 +1,17 @@
 import {NextRequest, NextResponse} from "next/server";
 import {createIssueSchema} from "@/app/validationSchema";
 import prisma from "@/prisma/client";
+import {getServerSession} from "next-auth";
+import authOptions from "@/app/auth/AuthOptions";
 
 export async function PATCH(
     request: NextRequest,
     {params}: {params: {id: string}}) {
+    const session = await getServerSession(authOptions)
+    if (!session) {
+        return NextResponse.json({}, {status: 401})
+    }
+
     const body = await request.json()
     const validation = createIssueSchema.safeParse(body)
 
@@ -31,6 +38,10 @@ export async function PATCH(
 }
 
 export async function DELETE(request: NextRequest, {params}: {params: {id: string}}) {
+    const session = await getServerSession(authOptions)
+    if (!session) {
+        return NextResponse.json({}, {status: 401})
+    }
     const issue = await prisma.issue.findUnique({
         where: {
             id: parseInt(params.id)
