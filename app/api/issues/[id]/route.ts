349f09c196ff52e3336c9d67bc30cd6ee3765a65
Alex <alekseiivanov.2016@inbox.ru>
2024-05-27 11:42:36 +0300

    create api

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,5 +1,5 @@
 import {NextRequest, NextResponse} from "next/server";
-import {createIssueSchema} from "@/app/validationSchema";
+import { patchIssueSchema } from "@/app/validationSchema";
 import prisma from "@/prisma/client";
 import {getServerSession} from "next-auth";
 import authOptions from "@/app/auth/AuthOptions";
@@ -13,10 +13,21 @@ export async function PATCH(
     }
 
     const body = await request.json()
-    const validation = createIssueSchema.safeParse(body)
+    const validation = patchIssueSchema.safeParse(body)
 
 
     if (!validation.success) return NextResponse.json(validation.error.format(), {status: 400})
+
+    const {assignedToUserId, title, description} = body;
+
+    if (assignedToUserId) {
+        const user = await prisma.user.findUnique({
+            where: {id: assignedToUserId}
+        })
+        if (!user) {
+            return NextResponse.json({error: 'not valid user'}, {status: 400})
+        }
+    }
 
     const issue = await prisma.issue.findUnique({
         where: {
@@ -29,8 +40,9 @@ export async function PATCH(
     const updatedIssue = await prisma.issue.update({
         where: {id: issue.id},
         data: {
-            title: body.title,
-            description: body.description
+            title,
+            description,
+            assignedToUserId
         }
     })
 
