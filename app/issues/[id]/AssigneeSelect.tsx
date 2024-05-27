5b45f22930f447bfc0faece2e9f1f3fd9020c40f
Alex <alekseiivanov.2016@inbox.ru>
2024-05-27 12:43:30 +0300

    assign issue to a user

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -5,8 +5,9 @@ import {User} from ".prisma/client";
 import axios from "axios";
 import {useQuery} from "@tanstack/react-query";
 import Skeleton from "react-loading-skeleton";
+import {Issue} from "@prisma/client";
 
-const AssigneeSelect = () => {
+const AssigneeSelect = ({ issue }: {issue: Issue} ) => {
     const {data: users, error, isLoading} =useQuery<User[]>({
         queryKey: ['users'],
         queryFn: () => axios.get('/api/users').then(res => res.data),
@@ -19,11 +20,16 @@ const AssigneeSelect = () => {
     if (error) return null
 
     return (
-        <Select.Root>
+        <Select.Root
+            defaultValue={issue.assignedToUserId || 'unassigned'}
+            onValueChange={(userId) => {
+            axios.patch('/api/issues/' + issue.id, {assignedToUserId: userId === 'unassigned' ? null : userId})
+        }}>
             <Select.Trigger placeholder={'Assign...'}/>
             <Select.Content>
                 <Select.Group>
                     <Select.Label>Suggestions</Select.Label>
+                    <Select.Item value="unassigned">Unassigned</Select.Item>
                     {users?.map(user => <Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>)}
                 </Select.Group>
             </Select.Content>
