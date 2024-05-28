5549b4677149777fa8e4ac2b4e170891296c9f14
Alex <alekseiivanov.2016@inbox.ru>
2024-05-28 17:59:28 +0300

    build IssueChart.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import LatestIssues from "@/app/LatestIssues";
 import IssueSummary from "@/app/IssueSummary";
 import prisma from "@/prisma/client";
+import IssueChart from "@/app/IssueChart";
 
 export default async function Home({searchParams}: {searchParams: {page: string}}) {
   const open = await prisma.issue.count({where: {status: 'OPEN'}})
   const closed = await prisma.issue.count({where: {status: 'CLOSED'}})
   const inProgress = await prisma.issue.count({where: {status: 'IN_PROGRESS'}})
   return (
-    <IssueSummary open={open} closed={closed} inProgress={inProgress}/>
+    <IssueChart open={open} closed={closed} inProgress={inProgress}/>
 
   )
 }
